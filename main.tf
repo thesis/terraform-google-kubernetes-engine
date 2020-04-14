3112ba30d67961d942676c6270040e2a8143f0ff
Kristen Borges <kb0rg@users.noreply.github.com>
2020-04-14 15:46:33 -0700

    Fix error in setting default value

--- main.tf
+++ main.tf
@@ -82,9 +82,10 @@ resource "google_container_node_pool" "a_gke_node_pool" {
     machine_type = "${lookup(var.gke_node_pool, "machine_type", "")}"
     disk_type    = "${lookup(var.gke_node_pool, "disk_type", "")}"
     disk_size_gb = "${lookup(var.gke_node_pool, "disk_size_gb", "")}"
-    oauth_scopes = ["${split(",", lookup(var.gke_node_pool, "oauth_scopes", ""))}"]
-    tags         = ["${split(",", lookup(var.gke_node_pool, "tags", ""))}"]
-    labels       = "${var.labels}"
+    oauth_scopes = ["${split(",", lookup(var.gke_node_pool, "oauth_scopes", "https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring"))}"]
+
+    tags   = ["${split(",", lookup(var.gke_node_pool, "tags", ""))}"]
+    labels = "${var.labels}"
 
     metadata {
       disable-legacy-endpoints = "true"
--- variables.tf
+++ variables.tf
@@ -84,9 +84,7 @@ variable "gke_node_pool" {
   description = ""
   type        = "map"
 
-  default = {
-    oauth_scopes = "https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring"
-  }
+  default = {}
 }
 
 /* Config maps descriptor variables.
